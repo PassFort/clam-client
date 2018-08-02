52cf184be71d43bbcfb52b5f6283cfb61aa3813a
Joseph Brailsford <joe@brailsford.xyz>
2018-08-02 18:56:02 +0100

    Updated formatting for examples.

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,7 +6,11 @@ use std::env;
 fn main() {
     if let Some(path) = env::args().nth(1) {
         let client = ClamClient::new("127.0.0.1", 3310).unwrap();
-        println!("Scan for '{}':\n\t{:?}\n", path, client.scan_path(&path, true).unwrap());
+        println!(
+            "Scan for '{}':\n\t{:?}\n",
+            path,
+            client.scan_path(&path, true).unwrap()
+        );
     } else {
         println!("USAGE: cargo run --example simple \"<file_path>\"");
     }
--- examples/stream.rs
+++ examples/stream.rs
@@ -2,8 +2,8 @@ extern crate clam_client;
 
 use clam_client::client::ClamClient;
 use clam_client::response::ClamScanResult;
-use std::fs::File;
 use std::env;
+use std::fs::File;
 
 fn main() {
     if let Some(path) = env::args().nth(1) {
@@ -11,15 +11,13 @@ fn main() {
         let file = File::open(&path).unwrap();
 
         match client.scan_stream(file) {
-            Ok(result) =>
-                match result {
-                    ClamScanResult::Ok => println!("File {} is OK!", path),
-                    ClamScanResult::Found(virus) => println!("Found virus: '{}' in {}", virus, path),
-                    ClamScanResult::Error(err) => println!("Received error from ClamAV: {}", err)
-                }
-            Err(e) => println!("A network error occurred whilst talking to ClamAV:\n{}", e)
+            Ok(result) => match result {
+                ClamScanResult::Ok => println!("File {} is OK!", path),
+                ClamScanResult::Found(virus) => println!("Found virus: '{}' in {}", virus, path),
+                ClamScanResult::Error(err) => println!("Received error from ClamAV: {}", err),
+            },
+            Err(e) => println!("A network error occurred whilst talking to ClamAV:\n{}", e),
         }
-
     } else {
         println!("USAGE: cargo run --example stream \"<file_path>\"");
     }
