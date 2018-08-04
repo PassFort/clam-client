2af96e5688c66c9ce062082b08fe95eefd0e359f
Joseph Brailsford <joe@brailsford.xyz>
2018-08-04 21:04:56 +0100

    Fixed issue with date parsing on VERSION command.

--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,6 @@
 //! The `response` module is responsibe for parsing the responses issued to use
 //! by ClamAV. To do so, it relies on two external crates, namely, `nom` and `chrono`.
-//! 
+//!
 //! All structs and enums derive `Debug` for ease of client send debugging and development.
 
 use chrono::{DateTime, TimeZone, Utc};
@@ -9,7 +9,7 @@ use error::ClamError;
 use std::str::FromStr;
 
 /// `ClamStats` provides all of the metrics that Clam provides via the `STATS` command
-/// as at version 0.100. 
+/// as at version 0.100.
 #[derive(Debug)]
 pub struct ClamStats {
     pub pools: u64,
@@ -41,10 +41,10 @@ pub struct ClamVersion {
 pub enum ClamScanResult {
     /// An `Ok` response means that Clam found no virus in the given file/directory.
     Ok,
-    /// A `Found` response means that Clam did find one or more virus('s) in the given file/directory, 
+    /// A `Found` response means that Clam did find one or more virus('s) in the given file/directory,
     /// the first value of `Found` is the location where the virus was found, and the second value is
-    /// the name of the virus detected. 
-    /// 
+    /// the name of the virus detected.
+    ///
     /// *Note*: When performing a stream scan, the location is redundant, and will always be `instream`.
     Found(String, String),
     /// An `Error` response means that Clam encountered an error whilst processing the request,
@@ -55,16 +55,18 @@ pub enum ClamScanResult {
 impl ClamScanResult {
     /// `ClamScanResult::parse` takes a Clam scan result string and parses into into a `Vec<ClamScanResult`.
     /// A vec must be used because Clam may scan multiple files in one request, or may encounter
-    /// multuple errors. 
-    /// 
+    /// multuple errors.
+    ///
     /// *Note*: If performing a stream scan, the result will be converted to a single `ClamScanResult` by
     /// the caller.
     pub fn parse<T: AsRef<str>>(s_string: T) -> Vec<ClamScanResult> {
-        s_string.as_ref().split('\0')
+        s_string
+            .as_ref()
+            .split('\0')
             .filter(|s| s != &"")
             .map(|s| {
                 if s.ends_with("OK") {
-                    return ClamScanResult::Ok
+                    return ClamScanResult::Ok;
                 }
 
                 if s.contains("FOUND") {
@@ -73,8 +75,8 @@ impl ClamScanResult {
                     let virus = split
                         .take_while(|s| !s.starts_with("FOUND"))
                         .collect::<String>();
-                        
-                    return ClamScanResult::Found(path, virus)
+
+                    return ClamScanResult::Found(path, virus);
                 }
 
                 ClamScanResult::Error(s.to_owned())
@@ -88,7 +90,11 @@ impl ClamVersion {
     /// into a strongly typed struct assuming it retains a standard format of
     /// `version tag/build no/publish datetime`
     pub fn parse(v_string: String) -> ClamResult<Self> {
-        let parts: Vec<String> = v_string.split('/').map(|s| s.to_owned()).collect();
+        let parts: Vec<String> = v_string
+            .trim_right_matches('\0')
+            .split('/')
+            .map(|s| s.to_owned())
+            .collect();
 
         if parts.len() != 3 {
             return Err(ClamError::InvalidData(v_string));
@@ -114,16 +120,16 @@ impl ClamVersion {
 
 impl ClamStats {
     /// `ClamStats::parse` takes a statistics output of the Clam `STATS` command and uses
-    /// nom to parse that into a strongly typed struct. 
-    /// 
-    /// Given that this is likely to be the most volatile area of returned data, it is likely 
-    /// that this will fail across different versions. This parses the data expected as of 
+    /// nom to parse that into a strongly typed struct.
+    ///
+    /// Given that this is likely to be the most volatile area of returned data, it is likely
+    /// that this will fail across different versions. This parses the data expected as of
     /// version 0.100.0. If it cannot parse the data, then the result is returned in its
     /// raw form insude `ClamError::InvalidData`.
     pub fn parse(s_string: &str) -> ClamResult<Self> {
         match parse_stats(s_string) {
             Ok(v) => Ok(v.1),
-            Err(_) => Err(ClamError::InvalidData(s_string.to_owned()))
+            Err(_) => Err(ClamError::InvalidData(s_string.to_owned())),
         }
     }
 }
@@ -173,7 +179,7 @@ mod tests {
     use chrono::prelude::*;
     use response;
 
-    static VERSION_STRING: &'static str = "ClamAV 0.100.0/24802/Wed Aug  1 08:43:37 2018";
+    static VERSION_STRING: &'static str = "ClamAV 0.100.0/24802/Wed Aug  1 08:43:37 2018\0";
     static STATS_STRING: &'static str = "POOLS: 1\n\nSTATE: VALID PRIMARY\nTHREADS: live 1  idle 0 max 12 idle-timeout 30\nQUEUE: 0 items\n\tSTATS 0.000394\n\nMEMSTATS: heap 9.082M mmap 0.000M used 6.902M free 2.184M releasable 0.129M pools 1 pools_used 565.979M pools_total 565.999M\nEND\0";
 
     #[test]
@@ -194,7 +200,11 @@ mod tests {
     fn test_version_parse_publish_dt() {
         let raw = VERSION_STRING.to_owned();
         let parsed = response::ClamVersion::parse(raw).unwrap();
-        assert_eq!(parsed.release_date, Utc.datetime_from_str("Wed Aug  1 08:43:37 2018", "%a %b %e %T %Y").unwrap());
+        assert_eq!(
+            parsed.release_date,
+            Utc.datetime_from_str("Wed Aug  1 08:43:37 2018", "%a %b %e %T %Y")
+                .unwrap()
+        );
     }
 
     #[test]
@@ -208,22 +218,39 @@ mod tests {
     fn test_result_parse_found() {
         let raw = "/some/file: SOME_BAD-Virus FOUND\0";
         let parsed = response::ClamScanResult::parse(raw);
-        assert_eq!(parsed[0], response::ClamScanResult::Found("/some/file".to_string(), "SOME_BAD-Virus".to_string()));
+        assert_eq!(
+            parsed[0],
+            response::ClamScanResult::Found("/some/file".to_string(), "SOME_BAD-Virus".to_string())
+        );
     }
 
     #[test]
     fn test_result_parse_multi_found() {
         let raw = "/some/file: SOME_BAD-Virus FOUND\0/some/other_file: SOME_V*BAD-Virus FOUND\0";
         let parsed = response::ClamScanResult::parse(raw);
-        assert_eq!(parsed[0], response::ClamScanResult::Found("/some/file".to_string(), "SOME_BAD-Virus".to_string()));
-        assert_eq!(parsed[1], response::ClamScanResult::Found("/some/other_file".to_string(), "SOME_V*BAD-Virus".to_string()));
+        assert_eq!(
+            parsed[0],
+            response::ClamScanResult::Found("/some/file".to_string(), "SOME_BAD-Virus".to_string())
+        );
+        assert_eq!(
+            parsed[1],
+            response::ClamScanResult::Found(
+                "/some/other_file".to_string(),
+                "SOME_V*BAD-Virus".to_string()
+            )
+        );
     }
 
     #[test]
     fn test_result_parse_error() {
         let raw = "/some/file: lstat() failed or some other random error\0";
         let parsed = response::ClamScanResult::parse(raw);
-        assert_eq!(parsed[0], response::ClamScanResult::Error("/some/file: lstat() failed or some other random error".to_string()));
+        assert_eq!(
+            parsed[0],
+            response::ClamScanResult::Error(
+                "/some/file: lstat() failed or some other random error".to_string()
+            )
+        );
     }
 
     #[test]
